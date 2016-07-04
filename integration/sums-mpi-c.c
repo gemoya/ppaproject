348cd7eb25cec825344ffb03579bf1ff532e6c46
Gonzalo Moya <gemoya@lisa.csrg.inf.utfsm.cl>
2016-07-04 17:45:20 -0400

    intentando

--- integration/sums-mpi-c.c
+++ integration/sums-mpi-c.c
@@ -48,18 +48,18 @@ int main(int argc, char **argv){
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-	double result[1];
-	double final[1];
+	double result;
+	double final;
 
 	N=(b-a+1)/numprocs;
 	resto = (b-a+1)%numprocs;
 	
 	recbuffer = (double *)malloc(sizeof(int)*((b-a+1)/numprocs));
 
-	
+	sendbuffer = (double *)malloc(sizeof(int)*(b-a+1-resto));
 	if (rank == 0){
 
-		sendbuffer = (double *)malloc(sizeof(int)*(b-a+1-resto));
+		//sendbuffer = (double *)malloc(sizeof(int)*(b-a+1-resto));
 		//fill buffer
 		x_i = a;
 		i=0;
@@ -70,36 +70,20 @@ int main(int argc, char **argv){
 			i++;
 		}
 		// pasando todo el buffer tal vez es mejor sin pasar todo el buffer
-		printf("pasando al scatter\n");
 
 	}
-	MPI_Scatter(sendbuffer,N,MPI_DOUBLE,recbuffer,N,MPI_DOUBLE,0,MPI_COMM_WORLD);
-	//debug
-	if (rank == 0){
-		for(i = 0; i<N; i++){
-			printf("PROC:%d, Elemento #%d, valor: %f\n", rank, i,recbuffer[i]);
-		}
-	}
-	if (rank == 1){
-		for(i = 0; i<N; i++){
-			printf("PROC:%d, Elemento #%d, valor: %f\n",rank, i,recbuffer[i]);
-		}
-	}
+	//MPI_Scatter(sendbuffer,N,MPI_DOUBLE,recbuffer,N,MPI_DOUBLE,0,MPI_COMM_WORLD);
+	MPI_Bcast(sendbuffer, (b-a)+1-resto, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-	printf("Llamando a trapezoidRule con %f\n", recbuffer[0]);
-	trapezoidRule(&recbuffer[0], &result[0]);
+	double wea = rank*N;
+	trapezoidRule( &wea, &result);
+
 	MPI_Reduce(&result, &final, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-	for (i = (b-a+1-resto); i<(b-a+1); i++){
-		if (i+1>=b-a+1){
-			final[0] = final[0] + f(sendbuffer[i]);
-		} else{
-			final[0] = final[0] + 2*f(sendbuffer[i]);
 
-		}
-
-	}
-
-	printf("El resultado final es %f\n", final[0]);
+	if (rank ==0 ) printf("El resultado final es %f\n", final);
+	
+	free(sendbuffer);
+	MPI_Finalize();
 
 
 	return 0;
